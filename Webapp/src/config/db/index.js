798d622162a509305e2cc7b386a8724b6775f0e5
Trung <qtrung811203@gmail.com>
2023-06-21 20:14:02 +0700

    Ngon nghe

--- Webapp/src/config/db/index.js
+++ Webapp/src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 function connect(){
 
     try{
-        mongoose.connect('mongodb+srv://quoctrung811:Imanumber1@cluster0.mgw0j34.mongodb.net/');
+        mongoose.connect('mongodb+srv://quoctrung811:Imanumber1@cluster0.mgw0j34.mongodb.net/Toys');
         console.log('Connect successfully')
     }
     catch (error)
